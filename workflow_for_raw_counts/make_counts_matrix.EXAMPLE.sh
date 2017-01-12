85c93cf25966c6aa17ebbb5eb8a2313ce5029e48
anna shcherbina <annashcherbina@gmail.com>
2017-01-11 18:11:35 -0800

    clarified make_counts_matrix.sh example

--- workflow_for_raw_counts/make_counts_matrix.EXAMPLE.sh
+++ workflow_for_raw_counts/make_counts_matrix.EXAMPLE.sh
@@ -1,7 +1,7 @@
-sh make_counts_matrix.sh earlyG1_R1_controls/out/align/pooled_pseudo_reps/ppr1/ sample1
-sh make_counts_matrix.sh earlyG1_R1_controls/out/align/pooled_pseudo_reps/ppr2/ sample2
-sh make_counts_matrix.sh earlyG1_R1_treated/out/align/pooled_pseudo_reps/ppr1/ sample3
-sh make_counts_matrix.sh earlyG1_R1_treated/out/align/pooled_pseudo_reps/ppr2/ sample4
-paste sample1 sample2 sample3 sample4 > counts.txt
-Rscript getDiff_multiFactor.r
+sh make_counts_matrix.sh treated/out/align/pooled_pseudo_reps/ppr1/ treated1
+sh make_counts_matrix.sh treated/out/align/pooled_pseudo_reps/ppr2/ treated2
+sh make_counts_matrix.sh control/out/align/pooled_pseudo_reps/ppr1/ control1
+sh make_counts_matrix.sh control/out/align/pooled_pseudo_reps/ppr2/ control2
+paste control1 control2 treated1 treated2 > counts.txt
+
 
--- workflow_for_raw_counts/make_counts_matrix.sh
+++ workflow_for_raw_counts/make_counts_matrix.sh
@@ -4,4 +4,4 @@ echo "path to output_file: $2"
 
 gzip -d $1*gz
 python shift_atac_tagalign.py $1*tagAlign $1/slopped
-bedtools coverage -counts -a merged_peaks/ppr.merged.bed -b $1/slopped | cut -f4 > $2
+bedtools coverage -counts -a ppr.merged.bed -b $1/slopped | cut -f4 > $2
